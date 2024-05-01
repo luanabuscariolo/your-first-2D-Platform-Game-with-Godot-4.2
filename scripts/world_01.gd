92c7b9774f6d29dfdc2e1d0284d3813a8c28767a
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-05-01 15:38:33 +0100

    Update Scene, player, and camera
    Adicionado fundo parallax, colisões entre player e tilemap

---
+++ scripts/world_01.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var player = $player as CharacterBody2D
+@onready var camera = $camera as Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player.follow_camera(camera)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
