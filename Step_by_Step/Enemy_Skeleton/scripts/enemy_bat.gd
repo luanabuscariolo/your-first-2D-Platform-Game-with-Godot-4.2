c492bd4eaab7be26f39cdf53899fddb250d4a719
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-05-09 11:33:27 +0100

    Update

--- Step_by_Step/Enemy_Skeleton/scripts/enemy_bat.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-var state = "fly"
-func animHasFinished():
-	if state == "damaged":
-		queue_free()
-@onready var anim = $anim
-func _ready():
-	anim.animation_finished.connect(animHasFinished)
--- Step_by_Step/Enemy_Skeleton/scripts/move_platform.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatableBody2D
-
-@onready var anim = $anim
-
-@export_enum("move_horizontal", "move_vertical") var move_direction = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	anim.assigned_animation = "move_horizontal" if move_direction == 0 else "move_vertical"
