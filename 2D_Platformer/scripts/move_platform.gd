90180d585866f58f2580b0fd162b5540ab137ce8
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-05-17 16:27:28 +0100

    Update - Platforms

--- 2D_Platformer/scripts/move_platform.gd
+++ 2D_Platformer/scripts/move_platform.gd
@@ -4,6 +4,5 @@ extends AnimatableBody2D
 
 @export_enum("move_horizontal", "move_vertical") var move_direction = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	anim.assigned_animation = "move_horizontal" if move_direction == 0 else "move_vertical"
--- Step_by_Step/Platforms/scripts/fall_platform.gd
+++ Step_by_Step/Platforms/scripts/fall_platform.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
 @onready var respawn_position = self.global_position
+@onready var anim = $anim
 
 func _on_body_detector_body_entered(body):
 	if body.name == "player":
+		await get_tree().create_timer(1).timeout
 		set_deferred("freeze", false)
 		$respawn_timer.start()
 
